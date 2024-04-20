f9e20eeec971ddc53d230f1f924ed5c5863d56ad
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-04-20 18:32:48 +0900

    updated UI

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class LocationScreen extends StatefulWidget {
@@ -34,6 +38,69 @@ class _LocationScreenState extends State<LocationScreen> {
                     width: 300,
                   ),
                 ),
+              ),
+              const Text(
+                'Tokyo',
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                '23°C | Sunny',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                      color: Colors.white.withOpacity(0.2),
+                      shape: BoxShape.rectangle,
+                    ),
+                    height: 300,
+                    width: 400,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(
+                            top: 20,
+                            left: 20,
+                            right: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.circular(
+                              10.0,
+                            ),
+                            color: Colors.white.withOpacity(0.2),
+                            shape: BoxShape.rectangle,
+                          ),
+                          height: 50,
+                          width: double.infinity,
+                          child: const Row(
+                            children: [],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
